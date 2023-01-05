812faba1d85f74589e715e9a7454ac11053c527b
Kamal Moumen <kamal.moumen@halton.com>
2023-01-05 14:25:38 +0100

    Changes to be committed: 	modified:   docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Machine Learning Docs'
+copyright = '2021, Kamal Moumen'
+author = 'Kamal Moumen'
 
 release = '0.1'
 version = '0.1.0'
